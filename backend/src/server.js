9809f43773d5bfcb7f7e1d1b29a521660cd7b2a9
VerdieCraig <craigverdie@gmail.com>
2025-06-24 17:20:28 -0400

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
@@ -12,11 +13,23 @@ console.log(process.env.MONGO_URI);
 
 const app  = express(); 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors()); // Enable CORS for all routes
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors()); // Enable CORS for all routes
+}
+
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(rateLimiter);
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
+  app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
+  });
+}
 
 connectDB().then(() => {
    app.listen(PORT, () => { 
